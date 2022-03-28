8581ff8fcf42e2f017e3d40d5c7bce38b9f5be69
Shreyas Murali <smurali@livevox.com>
2022-03-28 20:13:02 +0530

    initial

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
